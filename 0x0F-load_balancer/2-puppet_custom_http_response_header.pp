b993508036a55498581b9e4ca47cfbbfc14b0e6b
alantiren <tirenalan0@gmail.com>
2023-09-06 00:33:14 +0300

    tasks

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,48 +1,16 @@
 # Define a custom Nginx configuration for the X-Served-By header
 
-package { 'nginx':
-  ensure => installed,
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-file { '/var/www/html':
-  ensure => directory,
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file { '/var/www/html/404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page",
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => '
-    server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
-      add_header X-Served-By $hostname;
-      root /var/www/html;
-      index index.html index.htm;
-
-      location /redirect_me {
-        return 301 https//youtube.com/;
-      }
-
-      error_page 404 /404.html;
-      location /404 {
-        root /var/www/html;
-        internal;
-      }
-    }
-  ',
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
