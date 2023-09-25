18eac9e11e1f71233a8a8d11b4f08128403ec881
alantiren <122862761+alantiren@users.noreply.github.com>
2023-09-25 20:38:37 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,13 @@ import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
     base_url = "https://jsonplaceholder.typicode.com/"
 
     user_url = f"{base_url}users/{employee_id}"
@@ -28,12 +34,3 @@ def get_employee_todo_progress(employee_id):
 
     for task in completed_tasks:
         print(f"\t{task['title']}")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    get_employee_todo_progress(employee_id)
