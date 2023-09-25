ee79f56569499bd639408c3c3f734bf61263398c
alantiren <122862761+alantiren@users.noreply.github.com>
2023-09-25 20:12:12 +0300

    Revert "Update 0-gather_data_from_an_API.py"
    This reverts commit 4af1b8298deffaf3b8dd27e1b72883eceb104b77.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,12 +6,34 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+def get_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/"
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    user_url = f"{base_url}users/{employee_id}"
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+
+    todos_url = f"{base_url}todos"
+    todos_params = {"userId": employee_id}
+    todos_response = requests.get(todos_url, params=todos_params)
+    todos_data = todos_response.json()
+
+    completed_tasks = [task for task in todos_data if task["completed"]]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos_data)
+
+    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{
+        total_tasks}):")
+
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    get_employee_todo_progress(employee_id)
