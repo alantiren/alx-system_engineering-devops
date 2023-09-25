3c80686e10a02377bcb07c6c6ac9e29b6cc152e5
alantiren <122862761+alantiren@users.noreply.github.com>
2023-09-25 20:01:11 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -23,8 +23,8 @@ def get_employee_todo_progress(employee_id):
     num_completed_tasks = len(completed_tasks)
     total_tasks = len(todos_data)
 
-    print(f"Employee {employee_name} is done with tasks("
-      f"{num_completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{
+        total_tasks}):")
 
     for task in completed_tasks:
         print(f"\t{task['title']}")
